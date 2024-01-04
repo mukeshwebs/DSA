dfab395939994800c7ca2462b1222f981ecf39d7
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-01-04 23:19:53 +0530

    Update Gold Mine Problem.java

--- DP/Gold Mine Problem.java
+++ DP/Gold Mine Problem.java
@@ -5,9 +5,13 @@ Time Complexity: O(n * m)
 Space Complexity: O(n * m)
 */
 
-Note : test case failing : 1097 /1115
+Note : test case failing : 1097 /
+start mining from the last col and for last col value will be as it is and in last 2nd col for top row and most bottom row it can travel forward and \(top)  /(bottom)
+and for anywhere in between it can travel in 3 direction so everytime to get the max gold we will pick max of all possible movement and then add with the current values.
 
+at last simply check the first col of each row and find the max value, so that will be the answer.
 ---------------------------------------------------------------------------------------------------------------
+  
   
 class Solution {
     static int maxGold(int n, int m, int arr[][]) {
