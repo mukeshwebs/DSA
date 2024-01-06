ea6d5af25fd80b2db15d9634b370d91d00981ab1
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-01-06 15:01:50 +0530

    Create Notes.java

---
+++ DP/Notes.java
@@ -0,0 +1,6 @@
+## Explaination for why time complexity is exponential 
+
+Exponential time complexity typically arises when, for each recursive call, there are multiple recursive calls 
+(usually a constant number greater than 1). This branching factor leads to an exponential growth in the number of function calls.
+
+----------------------------------------
