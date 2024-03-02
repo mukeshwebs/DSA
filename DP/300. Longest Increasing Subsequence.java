34039b1396c1424f9bc32186040bca42a3286f9a
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-03-02 18:18:24 +0530

    Update 300. Longest Increasing Subsequence.java

--- DP/300. Longest Increasing Subsequence.java
+++ DP/300. Longest Increasing Subsequence.java
@@ -37,4 +37,27 @@ class Solution {
 
 //Tabulation
 
+//tabulation
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int []dp = new int[n+1];
+        return lengthOfLIS_LR(nums, dp);
+    }
+    
+    public static int lengthOfLIS_LR(int [] nums, int []dp){
+        int maxLen = 0;
+        for(int i=0; i<nums.length; i++){
+            dp[i] = 1;
+            for(int j=i-1; j>=0; j--){
+                if(nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            maxLen = Math.max(dp[i],maxLen);
+        }
+        return maxLen;
+    }
+}
+
 
