882b24c751cbda66519207a287cdf352f18c8d41
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-03-02 16:37:51 +0530

    Update 300. Longest Increasing Subsequence.java

--- DP/300. Longest Increasing Subsequence.java
+++ DP/300. Longest Increasing Subsequence.java
@@ -2,21 +2,29 @@ https://leetcode.com/problems/longest-increasing-subsequence/description/
 
 //Memoization
 
+Logic: LIS  - Longest increasing subsequence 
+    The approach used is straight forward, [10,9,2,5,3,7,101,18]
+    for every index waha tk ke increase subseuqneces find kr rhe hai 
+    ex: from the lengthOfLIS we are passing i 
+    intialliy i = 101
+    so in LIS_memo it will check the inceasing subseuneces till 101 so similarly 
+    we are finding the lis at each index and maitaining the maximum.
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int maxLength = 0;
         int[] dp = new int[nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
-            maxLength = Math.max(maxLength, LIS_memo(nums, i, dp));
+            maxLength = Math.max(maxLength, LIS_memo(nums, i, dp));   
         }
         return maxLength;
     }
     public static int LIS_memo(int[] nums, int ei, int[] dp) {
-        if (dp[ei] != 0) { // memo
+        if (dp[ei] != 0) { // memoization
             return dp[ei];
-        }
-        int maxLen = 1;
-        for (int i = ei - 1; i >= 0; i--) {
+        } 
+        int maxLen = 1;                              //maxLen is 1 bcz an element itself can be increase subsequence
+        for (int i = ei - 1; i >= 0; i--) {            //starting from that element and for increasing subq checking smaller in previous of that 
             if (nums[i] < nums[ei]) {
                 int recLen = LIS_memo(nums, i, dp);
                 maxLen = Math.max(maxLen, recLen + 1);
