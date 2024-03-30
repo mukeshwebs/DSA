3c34a67d4e1e288d9086ea3846d1a006c2c8a08a
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-03-31 00:57:31 +0530

    Update 03_board_path.java

--- DP/03_board_path.java
+++ DP/03_board_path.java
@@ -1,1 +1,41 @@
+/*
+Given A board and dice, count the number of ways in which thrown dice can reach to end board
+*/
 
+//memoization
+    public static int boardPath_memo(int sp, int ep, int[] dp) {
+        if (sp == ep) {
+            return dp[sp] = 1;
+        }
+
+        if (dp[sp] != 0)
+            return dp[sp];
+
+        int count = 0;
+        for (int dice = 1; dice <= 6 && sp + dice <= ep; dice++) {
+            count += boardPath_memo(sp + dice, ep, dp);
+        }
+
+        return dp[sp] = count;
+    }
+
+
+//Tabulation
+
+    public static int boardPath_tabu(int SP, int ep, int[] dp) {
+        for (int sp = ep; sp >= 0; sp--) {
+            if (sp == ep) {
+                dp[sp] = 1;
+                continue;
+            }
+
+            int count = 0;
+            for (int dice = 1; dice <= 6 && sp + dice <= ep; dice++) {
+                count += dp[sp + dice];// boardPath_memo(sp + dice, ep, dp);
+            }
+
+            dp[sp] = count;
+        }
+
+        return dp[SP];
+    }
