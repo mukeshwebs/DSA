78c644040c25b8186980205a9a5910c87fda0264
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-04-06 15:11:25 +0530

    Update 55. Jump Game.java

--- DP/55. Jump Game.java
+++ DP/55. Jump Game.java
@@ -11,14 +11,17 @@ if we are able to reach till the flag from the current index
 we will update the flag to current index
 Note: in if condition we are checking the current max jump from current index excedds the flag index
 */
-class Solution {
+
+
+  class Solution {
     public boolean canJump(int[] nums) {
-        int finalIdx = nums.length-1;
+        int finalIdx = nums.length-1; // Last index ko finalIdx mein store karte hain
         for(int idx=nums.length-2; idx>=0; idx--){
-            if(idx+nums[idx]>=finalIdx){
-                finalIdx = idx;
+            if(idx+nums[idx]>=finalIdx){ // Agar hum current index se final index tak jump kar sakte hain
+                finalIdx = idx; // Toh finalIdx ko current index se replace karte hain
             }
         }
-        return finalIdx==0;
+        return finalIdx==0; // Agar hum 0 par pahunch jaate hain, toh hum keh sakte hain ki hum jump kar sakte hain
     }
 }
+
