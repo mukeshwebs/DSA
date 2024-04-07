a806f7434e477a283a5a14f45f75447a9b91ca8d
Mukesh Yadav <mukeshyadavparty55@gmail.com>
2024-04-08 02:07:36 +0530

    update

--- DP/2_String_Set/1_Longest_Palindromic_Subsequence.java
+++ DP/2_String_Set/1_Longest_Palindromic_Subsequence.java
@@ -15,13 +15,29 @@ Explanation: One possible longest palindromic subsequence is "bb".
 
 TC: In the worst case, all possible pairs of indices (i, j) are considered, leading to a time complexity of O(n^2),
 SC: The maximum depth of the recursion is limited by the length of the input string. Hence, the overall space complexity is O(n^2).
-####################################################################################################
+--------------------------------------------------------------------------------------------------------------
+
+// 516. Longest Palindromic Subsequence
+public static int lpss(String s, int i, int j, int[][] dp) {
+    if (i >= j)
+        return dp[i][j] = (i == j ? 1 : 0);
+
+    if (dp[i][j] != 0)
+        return dp[i][j];
+
+    if (s.charAt(i) == s.charAt(j))
+        return dp[i][j] = lpss(s, i + 1, j - 1, dp) + 2;
+    else
+        return dp[i][j] = Math.max(lpss(s, i + 1, j, dp), lpss(s, i, j - 1, dp));
+}
+
+//with comments
 class Solution {
      public static int lpss(String s, int i, int j, int[][] dp) {
         if (i >= j)  //case of i>j is for input-> "aa"
             return dp[i][j] = (i == j ? 1 : 0);
 
-        if (dp[i][j] != 0) //memp
+        if (dp[i][j] != 0) //memo
             return dp[i][j];
 
         if (s.charAt(i) == s.charAt(j)) //both chars are same
@@ -43,4 +59,29 @@ class Solution {
 
         return ans;       
     }
+}
+
+//using gap strategy
+
+public static int lpss_DP(String s, int I, int J, int[][] dp) {
+    int n = s.length();
+    for (int gap = 0; gap < n; gap++) {
+        for (int i = 0, j = gap; j < n; i++, j++) {
+            if (i >= j) {
+                dp[i][j] = (i == j ? 1 : 0);
+                continue;
+            }
+
+            if (s.charAt(i) == s.charAt(j))
+                //agr do char equal hai to unka len 2 aur uske palindrome hone ke liye
+                //unke bich me jo characters hai unka palindrmic subsequence hme i+1, j-1 par milega
+                //ex: abbcca if a and a are equals then unke bich me jo len hai uska
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+
+            else
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[I][J];
 }
