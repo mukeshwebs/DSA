f231f5fb17f5caabbd08571647d3ed452dce101c
Mukesh Yadav <mukeshyadavparty55@gmail.com>
2024-04-08 01:18:25 +0530

    update

--- DP/1_TwoPointer_set/04_Climbing_Stairs.java
+++ DP/1_TwoPointer_set/04_Climbing_Stairs.java
@@ -26,10 +26,10 @@ public int climbStairsHelper(int n, int idx) {
     if (idx > n) { //if current 1 jump steps + 2 steps jumps exceed the last step then return 0 from it
         return 0;
     }
-//
-//    int way_a = climbStairsHelper(n, idx + 1);  //can jump 1 steps at a time
-//    int way_b = climbStairsHelper(n, idx + 2);  //can jump 2 steps at a time
-    return climbStairsHelper(n, idx + 1) + climbStairsHelper(n, idx + 2);
+
+    int way_a = climbStairsHelper(n, idx + 1);  //can jump 1 steps at a time
+    int way_b = climbStairsHelper(n, idx + 2);  //can jump 2 steps at a time
+    return way_a + way_b;
 }
 
 //dp - memoization 
