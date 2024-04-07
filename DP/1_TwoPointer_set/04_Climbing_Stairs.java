a25c3f1367e7b6673d1af91f0fdff49bd54d6472
Mukesh Yadav <mukeshyadavparty55@gmail.com>
2024-04-08 01:10:20 +0530

    update

--- DP/1_TwoPointer_set/04_Climbing_Stairs.java
+++ DP/1_TwoPointer_set/04_Climbing_Stairs.java
@@ -13,6 +13,11 @@ Explanation: There are two ways to climb to the top.
 2. 2 steps
  */
 
+/*
+TC:
+SC:
+ */
+
 public int climbStairsHelper(int n, int idx) {
     if (idx == n) { // if we reach to the last index, we found 1 ways
         return 1;
@@ -21,10 +26,10 @@ public int climbStairsHelper(int n, int idx) {
     if (idx > n) { //if current 1 jump steps + 2 steps jumps exceed the last step then return 0 from it
         return 0;
     }
-
-    int way_a = climbStairsHelper(n, idx + 1);  //can jump 1 steps at a time
-    int way_b = climbStairsHelper(n, idx + 2);  //can jump 2 steps at a time
-    return way_a + way_b;
+//
+//    int way_a = climbStairsHelper(n, idx + 1);  //can jump 1 steps at a time
+//    int way_b = climbStairsHelper(n, idx + 2);  //can jump 2 steps at a time
+    return climbStairsHelper(n, idx + 1) + climbStairsHelper(n, idx + 2);
 }
 
 //dp - memoization 
