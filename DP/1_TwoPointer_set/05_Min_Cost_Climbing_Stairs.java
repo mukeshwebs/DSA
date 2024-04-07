f942e47393eb3731ec752d4862d349d9be6db315
Mukesh Yadav <mukeshyadavparty55@gmail.com>
2024-04-08 00:25:08 +0530

    update

--- DP/1_TwoPointer_set/05_Min_Cost_Climbing_Stairs.java
+++ DP/1_TwoPointer_set/05_Min_Cost_Climbing_Stairs.java
@@ -2,27 +2,26 @@ https://leetcode.com/problems/min-cost-climbing-stairs/
 
 
 class Solution {
-    public static int minCostClimbingStairs(int n, int[] cost, int []dp) {
+    public static int minCostClimbingStairs(int n, int[] cost, int[] dp) {
         //base case 
-        if(n>= cost.length)
-        {
+        if (n >= cost.length) {
             return 0;
         }
-        if(dp[n] != -1)
-        {
+        if (dp[n] != -1) {
             return dp[n];
         }
-        int a=minCostClimbingStairs(n+1,cost, dp)+cost[n];
-        int b=minCostClimbingStairs(n+2,cost, dp)+cost[n];
-        dp[n]=Math.min(a, b);
+        int a = minCostClimbingStairs(n + 1, cost, dp) + cost[n];
+        int b = minCostClimbingStairs(n + 2, cost, dp) + cost[n];
+        dp[n] = Math.min(a, b);
         return dp[n];
     }
-    public static int minCostClimbingStairs(int [] cost){
+
+    public static int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int []dp = new int[n+1];
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
-        int ans1= minCostClimbingStairs(0,cost, dp); //step with index 0
-        int ans2= minCostClimbingStairs(1,cost, dp); //step with index 1
+        int ans1 = minCostClimbingStairs(0, cost, dp); //step with index 0
+        int ans2 = minCostClimbingStairs(1, cost, dp); //step with index 1
         return Math.min(ans1, ans2);
     }
 }
