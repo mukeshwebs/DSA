db1eab22aa38767bd59d280e2ef5d254f7ad8984
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-04-11 10:55:18 +0530

    Create 863. All Nodes Distance K in Binary Tree.java

---
+++ Trees/1_find_Set/863. All Nodes Distance K in Binary Tree.java
@@ -0,0 +1,44 @@
+https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+
+class Solution {
+     boolean nodeToRootPath_(TreeNode root, int data, ArrayList<TreeNode> ans) {
+
+        if (root == null)
+            return false;
+
+        if (root.val == data) {
+            ans.add(root);
+            return true;
+        }
+        boolean res = nodeToRootPath_(root.left, data, ans) || nodeToRootPath_(root.right, data, ans);
+
+        if (res)
+            ans.add(root);
+        return res;
+    }   
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        ArrayList<TreeNode> path = new ArrayList<>();
+        nodeToRootPath_(root, target.val, path);
+
+        List<Integer> ans = new ArrayList<>();
+        TreeNode block = null;
+        for (int i = 0; i < path.size(); i++) {
+            kdown(path.get(i), k - i, block, ans);
+            block = path.get(i);
+        }
+        return ans;
+    }
+     public void kdown(TreeNode root, int k, TreeNode block, List<Integer> ans) {
+        if (root == null || k < 0 || root == block)
+            return;
+
+        if (k == 0) {
+            ans.add(root.val);
+            return;
+        }
+
+        kdown(root.left, k - 1, block, ans);
+        kdown(root.right, k - 1, block, ans);
+    }   
+}
