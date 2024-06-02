086da453acbd2ba8d9df26b2df13b4ea1f2cd224
Mukesh Yadav <53649320+mukeshwebs@users.noreply.github.com>
2024-06-02 20:44:06 +0530

    Update Rotate Array.java

--- Arrays_&_Strings/Rotate Array.java
+++ Arrays_&_Strings/Rotate Array.java
@@ -1,2 +1,31 @@
 https://leetcode.com/problems/rotate-array/description/
 
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
+        if(k>nums.length){ //if k is greater than array length 
+            k = k%n;   
+        }
+        /*
+        ex: nums = [1,2,3,4], k = 5
+        Output: 
+        */
+        //3 times reverse
+        reverse(nums, 0, n-k-1);
+        reverse(nums, n-k, n-1);        
+        reverse(nums, 0, n-1); 
+    }
+    public static void reverse(int [] nums, int low, int high){
+        while(low<=high){
+            //swap the values 
+            int temp = nums[low];
+            nums[low] = nums[high];
+            nums[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
+
+
